115dbfc34da78f84dccac86bf6d48ef15fa6bc27
insolor <2442833+insolor@users.noreply.github.com>
2026-04-26 00:04:58 +0300

    Fix typos in the error message

--- ci/get_version.py
+++ ci/get_version.py
@@ -7,6 +7,6 @@ build_zig_zon = project_root_dir / "build.zig.zon"
 build_zig_zon_content = build_zig_zon.read_text()
 match = re.search(r"\.version\s*=\s*\"([\d\.]+)\"", build_zig_zon_content)
 if not match:
-    raise ValueError("Faild to get get version from build.zig.zon")
+    raise ValueError("Failed to get version from build.zig.zon")
 
 print(match.group(1))
