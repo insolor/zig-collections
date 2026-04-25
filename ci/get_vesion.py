5318f7ae8130ed77e3af0ba82e131a3590cbbb41
insolor <2442833+insolor@users.noreply.github.com>
2026-04-25 23:46:01 +0300

    Properly process failing of getting of version from build.zig.zon

--- ci/get_vesion.py
+++ ci/get_vesion.py
@@ -6,4 +6,7 @@ build_zig_zon = project_root_dir / "build.zig.zon"
 
 build_zig_zon_content = build_zig_zon.read_text()
 match = re.search(r"\.version\s*=\s*\"([\d\.]+)\"", build_zig_zon_content)
-print(match.group(1))
+if not match:
+    raise ValueError("Faild to get get version from build.zig.zon")
+
+print(match.group())
